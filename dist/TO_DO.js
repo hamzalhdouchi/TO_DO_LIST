c3f00cf7a42ad4b333b77dcb9ff85a1bc0206ada
hamzalhdouchi <hamzabniss16@gmail.com>
2024-11-02 17:30:23 +0100

    Déclaration des éléments et ajout d'ID pour toutes les pages.

---
+++ dist/TO_DO.js
@@ -0,0 +1,10 @@
+let titleInput = document.getElementById('title')
+let descriptionInput = document.getElementById('description');
+let infoInput = document.getElementById('info');
+let statusInput = document.getElementById('status');
+let st_dateInput = document.getElementById('st_date');
+let end_dateInput = document.getElementById('end_date');
+let popup = document.getElementById('popup');
+let todo= document.getElementById('todo');
+let doing = document.getElementById('Doing');
+let done = document.getElementById('Done');
