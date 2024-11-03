80af7fd130ab72c4f89f6a216b1d1b1afeb1423d
hamzalhdouchi <hamzabniss16@gmail.com>
2024-11-03 15:10:57 +0100

    Création de la fonction addTask pour ajouter de nouvelles tâches

--- dist/TO_DO.js
+++ dist/TO_DO.js
@@ -16,19 +16,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function togglePopup() {
     popup.classList.toggle('hidden');
 }
-
-let title = titleInput.value;
-const description = descriptionInput.value;
-const info = infoInput.value;
-const status = statusInput.value;
-const endDate = end_dateInput.value;
-
-// Example: You can now use these values to create a task object or append them to your task list
-const task = {
-    description,
-    status,
-    endDate,
-    title,
-    info,
-    id: Math.floor(Math.random() * 100) + 1
-};
+function addTask() {
+    rigex();
+    let title = titleInput.value;
+    const description = descriptionInput.value;
+    const info = infoInput.value;
+    const status = statusInput.value;
+    const endDate = end_dateInput.value;
+    
+    // Example: You can now use these values to create a task object or append them to your task list
+    const task = {
+        description,
+        status,
+        endDate,
+        title,
+        info,
+        id: Math.floor(Math.random() * 100) + 1
+    };
+    const data = localStorage.getItem("storeTicket");
+    let ticketsArray = [];
+    if (data) {
+        ticketsArray = JSON.parse(data);
+    }
+    ticketsArray.push(task);
+    localStorage.setItem("storeTicket", JSON.stringify(ticketsArray));
+    clearInputFields();
+    popup.classList.add("hidden");
+    location.reload();
+}
