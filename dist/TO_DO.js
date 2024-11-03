51bdd28e7da2228fa3cdff2d6372dff887547d42
hamzalhdouchi <hamzabniss16@gmail.com>
2024-11-03 18:39:57 +0100

    Mise en place de la validation des champs de saisie avec des expressions régulières.

--- dist/TO_DO.js
+++ dist/TO_DO.js
@@ -5,7 +5,7 @@ let statusInput = document.getElementById('status');
 let st_dateInput = document.getElementById('st_date');
 let end_dateInput = document.getElementById('end_date');
 let popup = document.getElementById('popup');
-let todo= document.getElementById('todo');
+let todo = document.getElementById('todo');
 let doing = document.getElementById('Doing');
 let done = document.getElementById('Done');
 
@@ -23,7 +23,7 @@ function addTask() {
     const info = infoInput.value;
     const status = statusInput.value;
     const endDate = end_dateInput.value;
-    
+
     const task = {
         description,
         status,
@@ -42,4 +42,35 @@ function addTask() {
     clearInputFields();
     popup.classList.add("hidden");
     location.reload();
+}
+
+function rigex() {
+    const titlePattern = /^[a-zA-Z\s]{3,30}$/;
+    const descPattern = /^[a-zA-Z0-9\s.,-]{10,100}$/;
+
+    if (!titlePattern.test(titleInput.value)) {
+        showAlert(
+            "Le titre doit être entre 3 et 30 caractères, seulement des lettres."
+        );
+        return false;
+    }
+
+    if (!descPattern.test(descriptionInput.value)) {
+        showAlert(
+            "La description doit être entre 10 et 100 caractères, avec seulement des lettres, chiffres, et ponctuations."
+        );
+        return false;
+    }
+
+    const currentDate = new Date();
+    const endDate = new Date(end_dateInput.value);
+
+    if (currentDate >= endDate || end_dateInput.value === '') {
+        showAlert(
+            "La date de fin doit être dans le futur par rapport à la date actuelle."
+        );
+        return false;
+    }
+
+    return true;
 }
